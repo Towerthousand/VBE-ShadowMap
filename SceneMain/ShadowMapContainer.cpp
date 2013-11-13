809b254a6fc8d6f4845a55eb3ee621a5f675f272
Dirbaio Minikiwi <dirbaio@dirbaio.net>
2013-11-13 04:08:07 +0100

    Finish! :D

--- SceneMain/ShadowMapContainer.cpp
+++ SceneMain/ShadowMapContainer.cpp
@@ -4,33 +4,38 @@
 
 ShadowMapContainer::ShadowMapContainer() : depthBuffer(nullptr) {
 	setName("smc");
-	depthBuffer = new RenderTarget();
-	depthBuffer->attachTexture(SCRWIDTH,SCRHEIGHT,RenderTarget::DEPTH,Texture::DEPTH_COMPONENT,2);
-	depthBuffer->getAttachedTexture(RenderTarget::DEPTH)->setFilter(GL_LINEAR,GL_LINEAR);
-	depthBuffer->getAttachedTexture(RenderTarget::DEPTH)->setComparison(GL_LESS);
-	depthBuffer->noDrawingTargets();
-	RenderTarget::bindScreen();
+
+	depthBuffer = new RenderTarget(SCRWIDTH, SCRHEIGHT);
+	depthBuffer->addTexture(RenderTarget::DEPTH, Texture::DEPTH_COMPONENT);
+	depthBuffer->build();
+
+	depthBuffer->getTextureForAttachment(RenderTarget::DEPTH)->setFilter(GL_LINEAR,GL_LINEAR);
+	depthBuffer->getTextureForAttachment(RenderTarget::DEPTH)->setComparison(GL_LESS);
+	RenderTarget::bind(nullptr);
 }
 
 ShadowMapContainer::~ShadowMapContainer() {
+	delete depthBuffer;
 }
 
 void ShadowMapContainer::draw() const {
 	std::vector<const ShadowModel*> targets;
 	this->getAllObjectsOfType<ShadowModel>(targets);
+
 	//DEPTH BUFFER
-	depthBuffer->use();
+	RenderTarget::bind(depthBuffer);
 	glClear(GL_DEPTH_BUFFER_BIT);
 	for(unsigned int i = 0; i < targets.size(); ++i)
 		targets[i]->drawMode = ShadowModel::ShadowMap;
 	ContainerObject::draw();
+
 	//REAL DRAWING
-	RenderTarget::bindScreen();
+	RenderTarget::bind(nullptr);
 	for(unsigned int i = 0; i < targets.size(); ++i)
 		targets[i]->drawMode = ShadowModel::Real;
 	ContainerObject::draw();
 }
 
 Texture*ShadowMapContainer::getDepthTexture() {
-	return depthBuffer->getAttachedTexture(RenderTarget::DEPTH);
+	return depthBuffer->getTextureForAttachment(RenderTarget::DEPTH);
 }
